9084356e05be9b79640cc4833739fb3bf0049039
Mohammed ashik <ashiqmohamed886@gmail.com>
2026-03-21 23:15:46 +0530

    add question for pattern 4

--- Patterns/pattern_4.py
+++ Patterns/pattern_4.py
@@ -1,3 +1,13 @@
+# Given an integer n. You need to recreate the pattern given below for any value of N.
+# Let's say for N = 5, the pattern should look like as below:
+
+
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
 class Solution:
     def pattern_4(self,n):
         for i in range(1, n+1):
