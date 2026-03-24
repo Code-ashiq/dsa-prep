d8706370fa9f9b243854d383a6188ba3f55b21e8
Mohammed ashik <ashiqmohamed886@gmail.com>
2026-03-24 22:12:14 +0530

    add question for pattern 6

--- Patterns/pattern_6.py
+++ Patterns/pattern_6.py
@@ -1,3 +1,12 @@
+# Given an integer n. You need to recreate the pattern given below for any value of N.
+# Let's say for N = 5, the pattern should look like as below:
+
+# 12345
+# 1234
+# 123
+# 12
+# 1
+
 class Solution:
     def pattern_6(self,n):
         for i in range(n, 0, -1):
